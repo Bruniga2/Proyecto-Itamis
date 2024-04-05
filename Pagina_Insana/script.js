d88509af985556591b0e460f89e1c7e63d1eeab4
Bruno Zuñiga <bru.zuniga@duocuc.cl>
2024-04-05 20:06:22 -0300

    insanidad 1.3

--- Pagina_Insana/script.js
+++ Pagina_Insana/script.js
@@ -1,43 +1,21 @@
 $(document).ready(function() {
-    // Inicializar el carrusel de recetas
-    $('.recipe-carousel').owlCarousel({
-      loop: true,
-      margin: 20,
-      nav: true,
-      responsive: {
-        0: {
-          items: 1
-        },
-        600: {
-          items: 2
-        },
-        1000: {
-          items: 4
-        }
+  $(".recipes-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: false,
+    responsive: {
+      0: {
+        items: 2
       },
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplayHoverPause: true
-    });
-  
-    // Cargar más productos
-    let productCount = 8; // Número inicial de productos
-    let loadMoreButton = $('.load-more');
-    loadMoreButton.click(function() {
-      let newProducts = '';
-      for (let i = 0; i < 4; i++) {
-        newProducts += `
-          <div class="product-card">
-            <img src="https://t1.uc.ltmcdn.com/es/posts/7/0/4/como_hacer_el_trago_destornillador_42407_orig.jpg" alt="Producto ${productCount + i + 1}">
-            <h3>Nombre del Producto ${productCount + i + 1}</h3>
-            <p>$ Precio del Producto ${productCount + i + 1}</p>
-          </div>
-        `;
+      600: {
+        items: 3 
+      },
+      1000: {
+        items: 5
+
       }
-      $('.product-grid').append(newProducts);
-      productCount += 4;
-      if (productCount >= 20) {
-        loadMoreButton.hide();
-      }
-    });
+    }
   });
+});
